1e1015188c79dab131768ed77dab71ec51432695
dim3662 <dim3662@yandex.ru>
2021-03-25 23:26:56 +0300

    DotNet Domain

---
+++ DotNetProjectDomain/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace DotNetProject
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+
+        public string message { get; }
+
+        public Message(string message)
+        {
+            this.message = message;
+        }
+
+        // static void Main(string[] args)
+        // {
+        //     Secret msg = new Secret();
+        //     msg.Message = new Message("asd");
+        //     Console.WriteLine("Hello");
+        //     Console.WriteLine(msg.Message.message);
+        // }
+    }
+}
---
+++ DotNetProjectDomain/Secret.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotNetProject
+{
+    public class Secret
+    {
+        public Guid Id { get; set; }
+        public string Password { get; set; }
+        public Message Message { get; set; }
+        public string Lifetime { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
